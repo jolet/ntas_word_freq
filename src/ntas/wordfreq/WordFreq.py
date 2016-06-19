468d796d18932f2467a998fe0e6ada761e017e0e
jkovacek <josipkovacek@yahoo.com>
2016-06-19 20:15:10 +0200

    cleanup

--- src/ntas/wordfreq/WordFreq.py
+++ src/ntas/wordfreq/WordFreq.py
@@ -1,45 +1,19 @@
 '''
 Created on 19. lip 2016.
 
-@author: josip.kovacek
+@author: jk
 '''
 # python-docx 
 from docx import Document
-# debug mode
-# import pdb
-# pdb.set_trace()
 
 # Predstavlja binarno stablo
-class Tree:
-    def __init__(self, root = None, left = None, right = None):
+class BinaryTree:
+    def __init__(self, root=None, left=None, right=None):
         self.root = root
         self.left = left
         self.right = right
-        self.frequency = 0
+        self.frequency = 1
         return
-    
-    def inorder(self):
-        temp_inorder=''
-#         temp_inorder_arr=[]
-        # navigate left tree
-        if self.left != None:
-            if temp_inorder:
-                temp_inorder += " " 
-            temp_inorder = temp_inorder + self.left.inorder()
-        
-        # navigate ROOT
-        if temp_inorder:
-            temp_inorder += " "
-        if self.root:
-            temp_inorder += self.root
-        
-        # navigate right tree
-        if self.right != None:
-            if temp_inorder:
-                temp_inorder += " "
-            temp_inorder = temp_inorder + self.right.inorder()
-        
-        return temp_inorder  
     
     def inorder_instant_print(self):
         # navigate left tree
@@ -47,17 +21,17 @@ class Tree:
             self.left.inorder_instant_print()
         
         # navigate ROOT
-        #if self.root != None:
-        print self.root,
-        
+        if self.root :
+            print '|{:2}. {:5}'.format(self.frequency, self.root),
+
         # navigate right tree
         if self.right != None:
             self.right.inorder_instant_print()
 
-# Kreira novi node i dodaje ga u binarno uredjeno stablo # from Tree import *
+# Kreira novi node i dodaje ga u binarno uredjeno stablo # from BinaryTree import *
 def createAndInsertNode(tree_ref, word):
 
-    word_node_ref = Tree(word)
+    word_node_ref = BinaryTree(word)
     if tree_ref.root == None:
         tree_ref.root = word_node_ref.root 
     else: 
@@ -65,7 +39,7 @@ def createAndInsertNode(tree_ref, word):
         while not added:
             if word <= tree_ref.root:
                 if word == tree_ref.root:
-    #                 print "same word: " + word
+                    # print "same word: " + word
                     tree_ref.frequency += 1
                     return tree_ref
                 if tree_ref.left == None:
@@ -80,35 +54,19 @@ def createAndInsertNode(tree_ref, word):
                     added = True
                 else:
                     tree_ref = tree_ref.right
-    #         else: 
-    #             tree_ref.frequency += 1;
-    #             print 'found duplicate'
-                        
-    #return tree_ref
 
 # Uklanja interpunkcije iz rijeci
 def normalizeWord(word):
-    #out = stringIn.translate(stringIn.maketrans("",""), string.punctuation)
-    return "".join(char for char in word if char not in ('!','.',',','?','(', ')', '"', '-', ':', ";"))
+    # out = stringIn.translate(stringIn.maketrans("",""), string.punctuation)
+    return "".join(char for char in word if char not in ('!', '.', ',', '?', '(', ')', '"', '-', ':', ";")).encode('utf-8').strip().upper()
 
 ###########################[WORD_FFREQUENCY_CALCULATOR]##############################
-print("Starting word frequency calculation...")
 document = Document('Homer2.docx')
-frequencyBST = Tree()
+frequencyBST = BinaryTree()
 
 for paragraph in document.paragraphs:
         for word in paragraph.text.split():
-            print (normalizeWord(word))
-            normalizedWord = normalizeWord(word).encode('utf-8').strip().upper()
-            #frequencyBST = 
-            createAndInsertNode(frequencyBST, normalizedWord)
+            createAndInsertNode(frequencyBST, normalizeWord(word))
 
 print "============================[INORDER]=========================\n"
-ino = frequencyBST.inorder().split()
-print 'unique words: ' + str(len(ino))
-for idx, val in enumerate(ino):
-    if idx % 7 == 0:
-        print "\n"
-    print '{:>4d}. {:15}'.format(idx + 1, val),
-print '\n------------------------------------------------------------------\n'
 frequencyBST.inorder_instant_print()    
